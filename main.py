b2d5b39460c98135c6385885f9c853925b1885b2
koftalofta6-star <koftalofta6@gmail.com>
2026-02-14 15:27:04 +0100

    Implement basic Telegram bot with start and echo commands

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
+
+TOKEN = os.getenv("TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("🤡 ClowBot est en ligne 24/7 !")
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(update.message.text)
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+print("Bot Railway démarré...")
+app.run_polling()
